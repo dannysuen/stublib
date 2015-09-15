fb1496bcbae7d6368d6e1e4f075aa6ba7a9915fe
孙昱 <suny@2345.com>
2015-09-15 14:15:35 +0800

    first commit

---
+++ stublib/src/main/java/me/dannysuen/stublib/IntentWhere.java
@@ -0,0 +1,14 @@
+package me.dannysuen.stublib;
+
+import android.content.Intent;
+
+/**
+ * Created by danny on 15-9-15.
+ */
+public class IntentWhere {
+
+    public static int findWhere(Intent intent) {
+        return 0;
+    }
+
+}
